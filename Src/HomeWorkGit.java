e7431baeff75e0297866e73f53088e9edbf8c0f4
ТфеÑNatalyaKUZYK <2598.nata.kuzyk@gmail.com>
2023-08-08 18:03:19 +0300

    Home Work with GIT2

--- Src/HomeWorkGit.java
+++ Src/HomeWorkGit.java
@@ -1,5 +1,6 @@
 public class HomeWorkGit {
     public static void main(String[]args){
         System.out.println("Hello git!!!");
+        System.out.println("Hello git from Natalya this is homework!!!");
     }
 }
